f2fecf69a0c38cf6e0fa34139e730feebad70989
ThomasCode92 <thomascode92@hotmail.com>
2024-02-27 19:10:06 +0100

    Sort

--- src/code/Stack.ts
+++ src/code/Stack.ts
@@ -1,9 +1,52 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
 
-    constructor() {}
+    private head?: Node<T>;
 
-    push(item: T): void {}
-    pop(): T | undefined {}
-    peek(): T | undefined {}
+    constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        const node: Node<T> = { value: item };
+
+        this.length++;
+
+        // first element added to the stack
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+
+        // free up memory - not necessary in JS
+        head.prev = undefined;
+
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
