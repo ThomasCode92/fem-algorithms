409481dcc06efe4edd3ffe1914220cb3f5f9ed52
ThomasCode92 <thomascode92@hotmail.com>
2024-02-27 19:09:08 +0100

    Queue

--- src/code/Queue.ts
+++ src/code/Queue.ts
@@ -1,9 +1,53 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
 
-    constructor() {}
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+
+    enqueue(item: T): void {
+        const node: Node<T> = { value: item };
+
+        this.length++;
+
+        // first element added to the queue
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    deque(): T | undefined {
+        if (!this.head) return undefined;
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free up memory - not necessary in JS
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
