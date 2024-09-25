b31e8aba7fa5f8481028f69f87e5aae6dfc66f77
ThomasCode92 <thomascode92@hotmail.com>
2024-09-25 07:58:38 +0200

    Implementing a Stack

--- src/Stack.ts
+++ src/Stack.ts
@@ -1,9 +1,46 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
+    private head?: Node<T>;
     public length: number;
 
-    constructor() {}
+    constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
 
-    push(item: T): void {}
-    pop(): T | undefined {}
-    peek(): T | undefined {}
+    push(item: T): void {
+        const node: Node<T> = { value: item };
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+        head.prev = undefined;
+
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
