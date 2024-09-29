86a2f1146dab1c06ca0e47a92c91e3459da41303
ThomasCode92 <thomascode92@hotmail.com>
2024-09-29 11:08:10 +0200

    Recursion

--- src/MazeSolver.ts
+++ src/MazeSolver.ts
@@ -1,1 +1,54 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {}
+const dir = [
+    [0, -1], // up
+    [1, 0], // right
+    [0, 1], // down
+    [-1, 0], // left
+];
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
+    // 1. Base case
+    // off the map
+    if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) return false;
+
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) return false;
+
+    // at the end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(curr);
+        return true;
+    }
+
+    // seen before
+    if (seen[curr.y][curr.x]) return false;
+
+    // 2. Recursive case
+    // // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // // recurse
+    for (let i = 0; i < dir.length; ++i) {
+        const [dx, dy] = dir[i];
+        const next = { x: curr.x + dx, y: curr.y + dy };
+        if (walk(maze, wall, next, end, seen, path)) return true;
+    }
+
+    // // post
+    path.pop();
+
+    return false;
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[i].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
+}
