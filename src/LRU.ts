abcc62e93425b855b4dd0176311bd11df7143ede
ThomasCode92 <thomascode92@hotmail.com>
2024-10-14 08:03:07 +0200

    LRU Cache Setup

--- src/LRU.ts
+++ src/LRU.ts
@@ -3,6 +3,17 @@ export default class LRU<K, V> {
 
     constructor() {}
 
-    update(key: K, value: V): void {}
-    get(key: K): V | undefined {}
+    update(key: K, value: V): void {
+        // does it exist?
+        // if it doesn't, we need to insert
+        //   - check capacity and evict if over
+        // if it does, we need to update to the front of the list
+        // and update the value
+    }
+
+    get(key: K): V | undefined {
+        // check the cache for existence
+        // update the value we found and move it to the front
+        // return out the value found or undefined if not exist
+    }
 }
